4fe760f70e9c6af2c4fb32faf94d507e84341fda
= <sujanthadarai710@gmail.com>
2025-12-23 17:09:14 +0545

    first change

---
+++ okey.py
@@ -0,0 +1,12 @@
+print("hello sipalaya")
+
+
+'''
+git init : local repo created 
+git add  : stage area
+git commit  :LOCAL repo(commit )
+git remote add : remote(setup)
+git push : remote repo
+
+
+'''
